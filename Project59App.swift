d66a79d0e2205821fdd8f07b07bf06bda3929d56
Maurice Salzmann <maurice.salzmann@stud.hslu.ch>
2023-11-13 19:18:13 +0100

    initial Project

---
+++ Project59App.swift
@@ -0,0 +1,17 @@
+//
+//  Project59App.swift
+//  Project59
+//
+//  Created by Maurice Salzmann on 13.11.23.
+//
+
+import SwiftUI
+
+@main
+struct Project59App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
