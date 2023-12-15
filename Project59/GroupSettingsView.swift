dd5dbb72a4c1c9496ac048804d1f0e6d1e491965
ts <thomas271@protonmail.com>
2023-12-15 18:55:05 +0100

    some fixes and refactoring

--- Project59/GroupSettingsView.swift
+++ Project59/GroupSettingsView.swift
@@ -9,17 +9,19 @@ import SwiftUI
 
 struct GroupSettingsView: View {
     @State var groupname: String = ""
-    @State var target: Float = 0
+    @State var target: Float = 100
     @State var start: Date = Date.now
     @State var stop: Date = Date.now
     @State var drinkslist:[DrinkSettingsView] = []
     @State var errorMessage: String = ""
     @State var drinkname: String = ""
-    @State var weight: Float = 0
+    @State var weight: Float = 1
+    
     
     @State var networking = Networking.shared
     
     @Environment(\.dismiss) var dismiss
+   
     
     func saveGroup() {
         Task {
@@ -40,57 +42,79 @@ struct GroupSettingsView: View {
     }
     
     var body: some View {
-        ScrollView {
-            VStack {
-                Text(errorMessage)
-                Text("Group Settings")
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                TextField("Groupname", text: $groupname)
-                TextField("Target", value: $target, format: .number)
-                DatePicker(
-                    "Start Time",
-                    selection: $start,
-                    displayedComponents: [.date]
-                )
-                DatePicker(
-                    "Stop Time",
-                    selection: $stop,
-                    displayedComponents: [.date]
-                )
-            }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding()
-            .background(Material.regular)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-            .shadow(color: .black.opacity(0.3), radius: 5)
-            .padding()
-            
-            VStack {
-                Text("Available Drinks")
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-               
-                    TextField("Drink", text: $drinkname)
-                    TextField("Weight", value: $weight, format: .number)
-                    Button("Add Drink") {
-                        drinkslist.append(DrinkSettingsView(drinkname: $drinkname.wrappedValue, weight: $weight.wrappedValue))
-                    }
+        NavigationStack{
+            ScrollView {
                 VStack {
-                    List(drinkslist, id: \.id) { drink in
-                        drink
+                    HStack{
+                        Text("Group")
+                        TextField("Groupname", text: $groupname).background(.white)
                     }
+                    
+                    HStack{
+                        Text("Target")
+                        TextField("Target", value: $target, format: .number).background(.white)
+                    }.hidden()
+                        DatePicker(
+                        "Start Time",
+                        selection: $start,
+                        displayedComponents: [.date]
+                    )
+                    DatePicker(
+                        "Stop Time",
+                        selection: $stop,
+                        displayedComponents: [.date]
+                    )
                 }
-                .frame(minWidth: 150, maxWidth: .infinity, minHeight: 300, maxHeight: .infinity)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .background(Material.regular)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .shadow(color: .black.opacity(0.3), radius: 5)
+                .padding()
+                
+                VStack {
+                    Text("Drink Options")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    HStack{
+                        VStack{
+                            Text("Drink") .frame(maxWidth: .infinity, alignment: .leading)
+                            TextField("Drink", text: $drinkname).background(.white)
+                        }    .frame(maxWidth: .infinity, alignment: .leading)
+                        VStack{
+                            Text("Weight") .frame(maxWidth: 70, alignment: .leading)
+                            TextField("Weight", value: $weight, format: .number).background(.white)
+                                .frame(maxWidth: 70, alignment: .leading)
+                        }
+                    }
+                    
+                   
+                    HStack{
+                        
+                       
+                        Button("Add Drink") {
+                            drinkslist.append(DrinkSettingsView(drinkname: $drinkname.wrappedValue, weight: $weight.wrappedValue))
+                        }
+                    }
+                    VStack {
+                        if drinkslist.count > 0{
+                            ForEach(drinkslist, id: \.id) { drink in
+                                drink
+                            }                 .frame(maxWidth: .infinity,  maxHeight: .infinity, alignment: .leading)
+                        }
+                    }
+   
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .background(Material.regular)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .shadow(color: .black.opacity(0.3), radius: 5)
+                .padding()
+            }.navigationTitle("Group Settings")
+            .toolbar {
+                Button("Save", action: saveGroup)
+                
             }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding()
-            .background(Material.regular)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-            .shadow(color: .black.opacity(0.3), radius: 5)
-            .padding()
-        }
-        .toolbar {
-            Button("Save", action: saveGroup)
-            
         }
     }
 }
